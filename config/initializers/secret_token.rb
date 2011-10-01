b91a602b9ab7bdb3dfdd3aa094096f8b98578d75
Amanda Powell <amanda@Nexus.(none)>
2011-10-01 17:39:46 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PublishingSolutions::Application.config.secret_token = '50b99edf9713dc64b1facc6c54f1712cee2866647dcc0ca26190ca946bcffb0fc9545438ec7d2476d0346bc2edea4f848bab47241ec24e87762893ba2e56e2b9'
